7b69353a6d170781889258b595ffeca24c39af58
vagrant <vagrant@nms>
2022-03-15 04:41:01 +0000

    networks.tf firstvpc

---
+++ tfm-workspace/networks.tf
@@ -0,0 +1,9 @@
+#Create VPC in ap-southeast-2
+resource "aws-vpc" "vpc_0001" {
+  provider             = aws.region-master
+  cidr_block           = "10.0.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = { Name = "my-first-vpc"
+  }
+}
