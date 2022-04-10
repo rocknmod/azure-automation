b72e7cfc62a5baf88a318d024ff146eb9bc701fc
remy <rocknmod@gmail.com>
2022-04-10 07:32:05 +0000

    terraform validate ok

--- tfm-workspace/modules/network/network.tf
+++ tfm-workspace/modules/network/network.tf
@@ -1,14 +1,14 @@
 # Terraform network.tf module
 # Set up alternate providers for network module
-terraform {
-  required_providers {
-    azurerm = {
-      source                = "hashicorp/azurerm"
-      version               = ">= 3.0.0"
-      configuration_aliases = [azurerm.sec]
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    azurerm = {
+#      source                = "hashicorp/azurerm"
+#      version               = ">= 3.0.0"
+#      configuration_aliases = [azurerm.sec]
+#    }
+#  }
+#}
 
 ###############
 #create the Ressource Group
@@ -16,12 +16,12 @@ terraform {
 
 resource "azurerm_resource_group" "tf_rg_syd" {
   name     = var.rg_name_syd
-  location = var.region-pri
+  location = var.region_pri
 }
 
 resource "azurerm_resource_group" "tf_rg_ldn" {
   name     = var.rg_name_ldn
-  location = var.region-sec
+  location = var.region_sec
 }
 
 ###############
@@ -31,19 +31,19 @@ resource "azurerm_resource_group" "tf_rg_ldn" {
 # Create virtual network in default Australia East
 
 resource "azurerm_virtual_network" "tf_vnet_syd" {
-  name                = "vpc_syd0001"
+  name                = "vnet_syd0001"
   resource_group_name = azurerm_resource_group.tf_rg_syd.name
   location            = azurerm_resource_group.tf_rg_syd.location
-  address             = var.network_ip
+  address_space             = var.network_ip
 }
 
 # Create virtual network in alernate UK South
 
 resource "azurerm_virtual_network" "tf_vnet_ldn" {
-  name                = "vpc_ldn0001"
+  name                = "vnet_ldn0001"
   resource_group_name = azurerm_resource_group.tf_rg_ldn.name
   location            = azurerm_resource_group.tf_rg_ldn.location
-  address             = var.network_ip
+  address_space             = var.network_ip
 }
 
 #end of VPC resource
--- tfm-workspace/modules/network/variables.tf
+++ tfm-workspace/modules/network/variables.tf
@@ -6,9 +6,26 @@ variable "location" {
   default     = null
 }
 
-variable "rg_name" {
+variable "region_pri" {
+  description = "Australia East"
+  type        = string
+  default     = "australiaeast"
+}
+
+variable "region_sec" {
+  description = "UK South"
+  type        = string
+  default     = "uksouth"
+}
+
+variable "rg_name_syd" {
   type    = string
-  default = null
+  default = "trf_rg_syd"
+}
+
+variable "rg_name_ldn" {
+  type    = string
+  default = "trf_rg_ldn"
 }
 
 variable "network_name" {
